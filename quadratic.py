f2aefc1a06bb1f350cffdf813bb4bb0f1124cd63
martibertolino <virginiacesarini195@gmail.com>
2026-05-18 13:59:36 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,34 @@
-# Replace the "ANSWER HERE" for your answer
+import math
 
 def roots(a, b, c):
-    return "ANSWER HERE"
-
+    discriminante = b ** 2 - 4 * a * c
+    if discriminante < 0:
+        return "( )"
+    if discriminante == 0:
+        r1 = -b / (2 * a)
+        return f"({r1})"
+    r1 = (-b + math.sqrt(discriminante)) / (2 * a)
+    r2 = (-b - math.sqrt(discriminante)) / (2 * a)
+    return f"({r1}, {r2})"
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
-
+    return a * (x ** 2) + b * x + c
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    if a == 0:
+        if b == 0:
+            return f"f(x) = {c}"
+        return f"f(x) = {b} * X + {c}"
+    if b == 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    if c == 0:
+        return f"f(x) = {a} * X^2 + {b} * X"
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
-
-def derivation(a, b, c):
-    return "ANSWER HERE"
+def derivation(a, b,c):
+    derivada_a = 2 * a
+    if derivada_a == 0:
+        return f"f'(x) = {b}"
+    if b == 0:
+        return f"f'(x) = {derivada_a} * X"
+    return f"f'(x) = {derivada_a} * X + {b}"
