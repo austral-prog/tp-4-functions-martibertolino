c5c0248da04032307fc209e017ca47c91b60ee8e
martibertolino <virginiacesarini195@gmail.com>
2026-05-18 14:04:08 -0300

    Update classify_number.py

--- classify_number.py
+++ classify_number.py
@@ -1,25 +1,19 @@
 # ---- Funciones provistas (NO modificar) ----
 
 def is_even(n):
-    """Dado un número entero n, retorna True si es par, False si es impar."""
     return n % 2 == 0
 
 def is_positive(n):
-    """Dado un número entero n, retorna True si es mayor a 0, False en caso contrario."""
     return n > 0
 
-# ---- Función a implementar ----
-
 def classify_number(n):
-    """
-    Dado un número entero n, retorna un string que lo clasifica.
-    Debe USAR las funciones is_even e is_positive para resolver el ejercicio.
-
-    Clasificaciones posibles:
-      - "positive even"   (positivo y par)
-      - "positive odd"    (positivo e impar)
-      - "negative even"   (negativo y par)
-      - "negative odd"    (negativo e impar)
-      - "zero"            (el número es 0)
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    if n == 0:
+        return "zero"
+    if is_positive(n):
+        result = "positive"
+    else:
+        result = "negative"
+    if is_even(n):
+        return f"{result} even"
+    else:
+        return f"{result} odd" 
